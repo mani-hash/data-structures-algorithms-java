36f1821f2e745ed3d45ebc5f75f877157619e8cb
mani-hash <manimehalan400@gmail.com>
2023-03-15 22:18:45 +0530

    Removed unnecessary files

--- Test.java
+++
@@ -1,9 +0,0 @@
-import java.util.LinkedList;
-
-public class Test {
-    public static void main(String[] args) {
-        LinkedList<String> names = new LinkedList<String>();
-
-        names.removeFirst();
-    }
-}
